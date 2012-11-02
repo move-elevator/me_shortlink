5b6f72b02ac4202f8ede979c877a841faf8f9702
Sascha Seyfert <sef@move-elevator.de>
2012-11-02 15:21:00 +0100

    #1929 Remove unused files, bugfix realUrl instance

--- Classes/Controller/ShortlinkController.php
+++ Classes/Controller/ShortlinkController.php
@@ -2,6 +2,9 @@
 
 namespace MoveElevator\MeShortlink\Controller;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Extension\ExtensionManager;
+
 /**
  * @author Sascha Seyfert <sef@move-elevator.de>
  * @package me_shortlink
@@ -9,8 +12,8 @@ namespace MoveElevator\MeShortlink\Controller;
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
-{
+class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+
     /**
      * @var \MoveElevator\MeShortlink\Domain\Repository\ShortlinkRepository
      * @inject
@@ -28,70 +31,93 @@ class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
      *
      * @return void
      */
-    public function redirectAction()
-    {
-        $requestUri = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
-        $httpHost = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
+    public function redirectAction() {
+	$requestUri = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+	$httpHost = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '';
 
-        $linkPath = pathinfo($httpHost . $requestUri);
-        $shortLinkToCheck = isset($linkPath['filename']) ? $linkPath['filename'] : '';
+	$linkPath = pathinfo($httpHost . $requestUri);
+	$shortLinkToCheck = isset($linkPath['filename']) ? $linkPath['filename'] : '';
 
-        $shortLinks = $this->shortlinkRepository->findByRequest($shortLinkToCheck);
+	$shortLinks = $this->shortlinkRepository->findByRequest($shortLinkToCheck);
 
-        $domains = $this->domainRepository->findByName($httpHost);
-        $domain = $domains->current();
+	$domains = $this->domainRepository->findByName($httpHost);
+	$domain = $domains->current();
 
-        if (is_object($shortLinks)) {
-            foreach ($shortLinks as $shortLink) {
-                if ($domain) {
-                    if ($domain->getPid() != $shortLink->getPid()) {
-                        continue;
-                    }
-                }
+	if (is_object($shortLinks)) {
+	    foreach ($shortLinks as $shortLink) {
+		if ($domain) {
+		    if ($domain->getPid() != $shortLink->getPid()) {
+			continue;
+		    }
+		}
 
-                if ($shortLink->getPage() != '') {
-                    if (\TYPO3\CMS\Core\Extension\ExtensionManager::isLoaded('realurl')) {
-                        $paramsTemp = explode('&', $shortLink->getParams());
-                        $params = array();
-                        foreach ($paramsTemp as $param) {
-                            $paramValue = explode('=', $param);
-                            if (isset($paramValue[0]) && isset($paramValue[1]))
-                                $params[$paramValue[0]] = $paramValue[1];
-                        }
-                        $url = $this->getSpeakingUrlFromRealUrl($shortLink->getPage(), $params);
-
-                    } else {
-                        $url = 'index.php?id=' . $shortLink->getPage() . $shortLink->getParams();
-                    }
-                    $this->redirectToPage(\TYPO3\CMS\Core\Utility\GeneralUtility::locationHeaderUrl($url));
-                } else if ($shortLink->getUrl()) {
-                    $this->redirectToPage($shortLink->getUrl());
-                }
-            }
-        }
+		$url = $this->getRedirectUrl($shortLink);
+		if ($url) {
+		    $this->redirectToPage($url);
+		}
+	    }
+	}
     }
 
-    protected function redirectToPage($url)
-    {
-
-        header("HTTP/1.0 301 Moved Permanently");
-        header("Status: 301 Moved Permanently");
-        header("Location: " . $url, true, 301);
-        exit;
+    /**
+     * Get url from Shortlink
+     * @param \MoveElevator\MeShortlink\Domain\Model\Shortlink
+     * @return string
+     */
+    protected function getRedirectUrl($shortLink) {
+	if ($shortLink->getPage() != '') {
+	    if (ExtensionManager::isLoaded('realurl')) {
+		$paramsTemp = explode('&', $shortLink->getParams());
+		$params = array();
+		foreach ($paramsTemp as $param) {
+		    $paramValue = explode('=', $param);
+		    if (isset($paramValue[0]) && isset($paramValue[1]))
+			$params[$paramValue[0]] = $paramValue[1];
+		}
+		$url = $this->getSpeakingUrlFromRealUrl($shortLink->getPage(), $params);
+	    } else {
+		$url = 'index.php?id=' . $shortLink->getPage() . $shortLink->getParams();
+	    }
+	    $url = GeneralUtility::locationHeaderUrl($url);
+	} else if ($shortLink->getUrl()) {
+	    $url = $shortLink->getUrl();
+	}
+	return $url;
     }
 
-    protected function getSpeakingUrlFromRealUrl($pid, $params = array())
-    {
-        $realUrl = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('tx_realurl');
-        $GLOBALS['TSFE']->sys_page = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('t3lib_pageSelect');
-        $GLOBALS['TSFE']->tmpl = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('t3lib_TStemplate');
-        $GLOBALS['TSFE']->config['config']['tx_realurl_enable'] = 1;
-        $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'pages', 'uid = ' . (int)$pid);
-        $pageRow = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
-        $conf['LD'] = $GLOBALS['TSFE']->tmpl->linkData($pageRow, '', 0, 'index.php', '', \TYPO3\CMS\Core\Utility\GeneralUtility::implodeArrayForUrl('', $params));
-        $realUrl->encodeSpURL($conf, $this);
-        $url = $conf['LD']['totalURL'];
-        return $url;
+    /**
+     * Redirect to url by 301
+     * @param string $url
+     * @return void
+     */
+    protected function redirectToPage($url) {
+	header("HTTP/1.0 301 Moved Permanently");
+	header("Status: 301 Moved Permanently");
+	header("Location: " . $url, true, 301);
+	exit;
+    }
+
+    /**
+     * Get Speaking path from RealUrl Extension
+     * @param integer $pid
+     * @param array $params
+     * @return void
+     */
+    protected function getSpeakingUrlFromRealUrl($pid, $params = array()) {
+	$GLOBALS['TSFE']->sys_page = GeneralUtility::makeInstance('t3lib_pageSelect');
+	$GLOBALS['TSFE']->tmpl = GeneralUtility::makeInstance('t3lib_TStemplate');
+	$GLOBALS['TSFE']->config['config']['tx_realurl_enable'] = 1;
+
+	$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', 'pages', 'uid = ' . (int) $pid);
+	$pageRow = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res);
+	if ($pageRow) {
+	    $conf['LD'] = $GLOBALS['TSFE']->tmpl->linkData($pageRow, '', 0, 'index.php', '', GeneralUtility::implodeArrayForUrl('', $params));
+	}
+
+	$realUrl = new \tx_realurl();
+	$realUrl->encodeSpURL($conf, $this);
+	$url = $conf['LD']['totalURL'];
+	return $url;
     }
 
 }
