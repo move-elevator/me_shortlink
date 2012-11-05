3fbcc0024a7f2183f78177dc79eff5f266952f97
Sascha Seyfert <sef@move-elevator.de>
2012-11-05 14:45:21 +0100

    #1929 format code

--- Classes/Domain/Repository/DomainRepository.php
+++ Classes/Domain/Repository/DomainRepository.php
@@ -11,10 +11,10 @@ namespace MoveElevator\MeShortlink\Domain\Repository;
  *
  */
 class DomainRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
-public function findByDomainName($domain) {
-        $query = $this->createQuery();
-        $query->getQuerySettings()->setRespectStoragePage(FALSE);
-	$query->matching($query->equals('name', $domain ,TRUE));
-        return $query->execute();
+    public function findByDomainName($domain) {
+	$query = $this->createQuery();
+	$query->getQuerySettings()->setRespectStoragePage(FALSE);
+	$query->matching($query->equals('name', $domain, TRUE));
+	return $query->execute();
     }
 }
