3a2c5a9107824960074f0724338efd96dd74d055
Sascha Seyfert <sef@move-elevator.de>
2012-12-14 08:25:08 +0100

    #1929 format code

--- Tests/Unit/Domain/Repository/ShortlinkRepositoryTest.php
+++ Tests/Unit/Domain/Repository/ShortlinkRepositoryTest.php
@@ -13,7 +13,7 @@ class ShortlinkRepositoryTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase
      * @var  \MoveElevator\MeShortlink\Domain\Repository\ShortlinkRepository
      */
     protected $repositoryObject;
-    
+
     /*
      * @var array
      */
@@ -21,37 +21,38 @@ class ShortlinkRepositoryTest extends \TYPO3\CMS\Extbase\Tests\Unit\BaseTestCase
 
     public function setUp() {
         $this->testingFramework = new \Tx_Phpunit_Framework('tx_meshortlink');
-	$this->testConfig = array(
-	    'title' => 'ShortlinkRepositoryTest',
-	    'page' => 1,
-	    'url' => '',
-	    'params' => '&foo=bar'
-	);
-	
-	$this->repositoryObject = $this->objectManager->get('\\MoveElevator\\MeShortlink\\Domain\\Repository\\ShortlinkRepository');
+        $this->testConfig = array(
+            'title' => 'ShortlinkRepositoryTest',
+            'page' => 1,
+            'url' => '',
+            'params' => '&foo=bar'
+        );
+
+        $this->repositoryObject = $this->objectManager->get('\\MoveElevator\\MeShortlink\\Domain\\Repository\\ShortlinkRepository');
         $this->testingFramework->createRecord(
-                'tx_meshortlink_domain_model_shortlink', array (
-		    'title' => $this->testConfig['title'],
-		    'page' => $this->testConfig['page'],
-		    'url' => $this->testConfig['url'],
-		    'params' => $this->testConfig['params'],
-		)
+                'tx_meshortlink_domain_model_shortlink', array(
+                    'title' => $this->testConfig['title'],
+                    'page' => $this->testConfig['page'],
+                    'url' => $this->testConfig['url'],
+                    'params' => $this->testConfig['params'],
+                )
         );
     }
 
     public function tearDown() {
-	$this->testingFramework->cleanUp();
-	unset($this->testingFramework);
+        $this->testingFramework->cleanUp();
+        unset($this->testingFramework);
         unset($this->repositoryObject);
     }
 
     public function testFindByRequest() {
-	$querySettings =new \TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings();
-	$querySettings->setRespectStoragePage(FALSE);
-	$this->repositoryObject->setDefaultQuerySettings($querySettings);
-	$shortlinkObject = $this->repositoryObject->findByRequest($this->testConfig['title'])->current();
+        $querySettings = new \TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings();
+        $querySettings->setRespectStoragePage(FALSE);
+        $this->repositoryObject->setDefaultQuerySettings($querySettings);
+        $shortlinkObject = $this->repositoryObject->findByRequest($this->testConfig['title'])->current();
         $this->assertEquals($shortlinkObject->getTitle(), $this->testConfig['title']);
     }
+
 }
 
 ?>
