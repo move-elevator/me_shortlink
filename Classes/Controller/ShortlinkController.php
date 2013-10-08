9207e807981cbff032cd1bf8eeb2d4433115c66e
Sascha Seyfert <sef@move-elevator.de>
2013-10-08 14:46:06 +0200

    #5335 format code

--- Classes/Controller/ShortlinkController.php
+++ Classes/Controller/ShortlinkController.php
@@ -31,7 +31,7 @@ class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
         $shortLinkToCheck = ShortlinkUtility::getValidShortlink($requestUri);
 
         if ($shortLinkToCheck !== FALSE) {
-            if(!isset($GLOBALS['TCA']['tx_meshortlink_domain_model_shortlink'])){
+            if (!isset($GLOBALS['TCA']['tx_meshortlink_domain_model_shortlink'])) {
                 $GLOBALS['TSFE']->includeTCA();
             }
             $shortLinks = $this->shortlinkRepository->findByRequest($shortLinkToCheck);
@@ -41,41 +41,44 @@ class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
             }
         }
     }
-    
+
     /**
      * check if shortlink matches against domain and redirect
+     *
      * @param \TYPO3\CMS\Extbase\Persistence\Generic\QueryResult $shortLinks
      * @param \MoveElevator\MeShortlink\Domain\Model\Domain $domain|NULL
      */
-    protected function checkShortLinksDomain(QueryResult $shortLinks, $domain = NULL){
+    protected function checkShortLinksDomain(QueryResult $shortLinks, $domain = NULL) {
         foreach ($shortLinks as $shortLink) {
             if ($domain && $domain->getPid() != $shortLink->getPid()) {
                 continue;
             }
-            
+
             $this->redirect($shortLink);
         }
     }
-    
+
     /**
      * redirect to shortlink target
+     *
      * @param \MoveElevator\MeShortlink\Domain\Model\Shortlink $shortLink
      * @return void
      */
-    protected function redirect($shortLink){
+    protected function redirect($shortLink) {
         $url = ShortlinkUtility::getRedirectUrlFromShortlink($shortLink);
-        
+
         if (GeneralUtility::isValidUrl($url)) {
             HttpUtility::redirect($url, HttpUtility::HTTP_STATUS_301);
         }
     }
-    
+
     /**
      * return ShortlinkDomain by given httpHost
+     *
      * @param string $httpHost
      * @return string $domain
      */
-    protected function getDomain($httpHost){
+    protected function getDomain($httpHost) {
         $querySettings = $this->objectManager->get('TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings');
         $querySettings->setIgnoreEnableFields(TRUE);
         $querySettings->setRespectStoragePage(FALSE);
@@ -83,7 +86,7 @@ class ShortlinkController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 
         $domains = $this->domainRepository->findByName($httpHost);
         $domain = $domains->current();
-        
+
         return $domain;
     }
 }
