b7e1991427377ba7b09b274df2403744bcacd941
Sascha Seyfert <sef@move-elevator.de>
2014-04-09 11:55:19 +0200

    #12639 fix ext_tables.sql for admin tool

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,41 +2,39 @@
 # Table structure for table 'tx_meshortlink_domain_model_shortlink'
 #
 CREATE TABLE tx_meshortlink_domain_model_shortlink (
-  uid             INT(11)                         NOT NULL AUTO_INCREMENT,
-  pid             INT(11) DEFAULT '0'             NOT NULL,
-  title           VARCHAR(255) DEFAULT ''         NOT NULL,
-  page            INT(11) DEFAULT '0'             NOT NULL,
-  url             VARCHAR(255) DEFAULT ''         NOT NULL,
-  params          VARCHAR(255) DEFAULT ''         NOT NULL,
-  is_dummy_record TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  tstamp          INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  crdate          INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  cruser_id       INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  deleted         TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  hidden          TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  starttime       INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  endtime         INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) DEFAULT '0' NOT NULL,
+  title varchar(255) DEFAULT '' NOT NULL,
+  page int(11) DEFAULT '0' NOT NULL,
+  url varchar(255) DEFAULT '' NOT NULL,
+  params varchar(255) DEFAULT '' NOT NULL,
+  is_dummy_record tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+  crdate int(11) unsigned DEFAULT '0' NOT NULL,
+  cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+  deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  starttime int(11) unsigned DEFAULT '0' NOT NULL,
+  endtime int(11) unsigned DEFAULT '0' NOT NULL,
   PRIMARY KEY (uid),
   KEY parent (pid),
-
 );
 
 #
 # Table structure for table 'tx_meshortlink_domain_model_domain'
 #
 CREATE TABLE tx_meshortlink_domain_model_domain (
-
-  uid             INT(11)                         NOT NULL AUTO_INCREMENT,
-  pid             INT(11) DEFAULT '0'             NOT NULL,
-  name            VARCHAR(255) DEFAULT ''         NOT NULL,
-  is_dummy_record TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  tstamp          INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  crdate          INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  cruser_id       INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  deleted         TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  hidden          TINYINT(4) UNSIGNED DEFAULT '0' NOT NULL,
-  starttime       INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
-  endtime         INT(11) UNSIGNED DEFAULT '0'    NOT NULL,
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) DEFAULT '0' NOT NULL,
+  name varchar(255) DEFAULT '' NOT NULL,
+  is_dummy_record tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+  crdate int(11) unsigned DEFAULT '0' NOT NULL,
+  cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+  deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+  starttime int(11) unsigned DEFAULT '0' NOT NULL,
+  endtime int(11) unsigned DEFAULT '0' NOT NULL,
   PRIMARY KEY (uid),
   KEY parent (pid)
 );
