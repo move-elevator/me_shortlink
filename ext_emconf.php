07d95d2ef9653e2ab46982c44440bb0f7702f064
Sascha Seyfert <sef@move-elevator.de>
2015-09-25 10:14:45 +0200

    increase supported version

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.4.5',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.0.0-7.7.99',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
