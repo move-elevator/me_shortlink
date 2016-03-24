d54001589b67042128f909e337e680c52f236a93
move:elevator (Jan Männig) <jma@move-elevator.de>
2016-03-24 15:05:40 +0100

    KOET-14 [tmp] remove test

--- Tests/Unit/Utility/ShortlinkUtilityTest.php
+++ Tests/Unit/Utility/ShortlinkUtilityTest.php
@@ -69,37 +69,4 @@ class ShortlinkUtilityTest extends UnitTestCase
         $this->assertFalse($this->utilityObject->getValidShortlink('*foo-bar_batz23.html'));
         $this->assertSame('foo-bar_batz23', ShortlinkUtility::getValidShortlink('foo-bar_batz23'));
     }
-
-    /**
-     * @covers \MoveElevator\MeShortlink\Utility\ShortlinkUtility::getInternalUrlFromShortlink
-     * @return void
-     */
-    public function testGetInternalUrlFromShortlink()
-    {
-        $GLOBALS['TSFE'] = $this->initializeFrontendConfiguration(1);
-
-        $this->fixtureShortlink['page'] = 1;
-        $this->assertStringStartsWith(
-            'http',
-            $this->utilityObject->getInternalUrlFromShortlink($this->fixtureShortlink)
-        );
-    }
-
-    protected static function initializeFrontendConfiguration($pageId)
-    {
-        /** @var \TYPO3\CMS\Extbase\Object\ObjectManager $objectManager */
-        $objectManager = GeneralUtility::makeInstance('TYPO3\CMS\Extbase\Object\ObjectManager');
-
-        $GLOBALS['TSFE'] = $objectManager->get(
-            'TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController',
-            $GLOBALS['TYPO3_CONF_VARS'],
-            $pageId,
-            0
-        );
-
-        $GLOBALS['TSFE']->initFEuser();
-        $GLOBALS['TSFE']->initTemplate();
-        Bootstrap::getInstance()->loadCachedTca();
-        $GLOBALS['TSFE']->cObj = $objectManager->get('TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer');
-    }
 }
